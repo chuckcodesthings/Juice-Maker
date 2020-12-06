377f918ef19b62a8525adb1065d474a7a4155cb6
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-06 15:55:38 -0500

    Fixed leftover parenthesis

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ setTimeout(function () {
     ? window.alert('You made apple juice, but it strangely tastes like the milk your uncle gives you when you visit him...')
     : apples > 0 && oranges > 0 && cum > 0
     ? window.alert('You made apple-orange juice, but it has a noticeable cum aftertaste!')
-    : apples == 0 && oranges > 0 && cum > 0)
+    : apples == 0 && oranges > 0 && cum > 0
     ? window.alert('You mixed semen with orange juice, go to hell!')
-    : apples == 0 && oranges == 0 && cum > 0)
+    : apples == 0 && oranges == 0 && cum > 0
     ? window.alert('You made a delicious glass of cum')
     : window.alert('Congrats, you made nothing!');
   
