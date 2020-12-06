b1ddce1a2a0c0fa6fa9837080dbefcef2c244698
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-06 15:33:56 -0500

    Updated code
    Used ternary operators instead of if/else statements that are hard to read lmao

--- script.js
+++ script.js
@@ -4,22 +4,19 @@ setTimeout(function () {
   let oranges = prompt(`Enter amount of oranges`);
   let cum = prompt(`Enter amount of cum in liters`);
 
-  if (apples > oranges && oranges == 0 && cum == 0) {
-    window.alert(`You made apple juice!`);
-  } else if (apples > 0 && oranges >= 0 && cum == 0) {
-    window.alert(`You made apple-orange juice!`);
-  } else if (apples == 0 && oranges > 0 && cum == 0) {
-    window.alert(`You made orange juice!`);
-  } else if (apples > oranges && oranges == 0 && cum > 0) {
-    window.alert(`You made apple juice, but it strangely tastes like the milk your uncle gives you when you visit him!`);
-  } else if (apples > 0 && oranges > 0 && cum > 0) {
-    window.alert(`You made apple-orange juice, but it has a noticeable cum aftertaste!`);
-  } else if (apples == 0 && oranges > 0 && cum > 0) {
-    window.alert(`You mixed semen with orange juice, go to hell!`);
-  } else if (apples == 0 && oranges == 0 && cum > 0) {
-    window.alert(`You made a delicious glass of cum`);
-  } else {
-    window.alert(`Congrats, you made nothing!`);
-  }
-
+  apples > oranges && oranges == 0 && cum == 0
+    ? window.alert(`You made apple juice!`);
+    : apples > 0 && oranges >= 0 && cum == 0
+    ? window.alert(`You made apple-orange juice!`)
+    : apples == 0 && oranges > 0 && cum == 0
+    ? window.alert(`You made orange juice!`)
+    : apples > oranges && oranges == 0 && cum > 0
+    ? window.alert(`You made apple juice, but it strangely tastes like the milk your uncle gives you when you visit him...`)
+    : apples > 0 && oranges > 0 && cum > 0
+    ? window.alert(`You made apple-orange juice, but it has a noticeable cum aftertaste!`)
+    : apples == 0 && oranges > 0 && cum > 0)
+    ? window.alert(`You mixed semen with orange juice, go to hell!`)
+    : apples == 0 && oranges == 0 && cum > 0)
+    ? window.alert(`You made a delicious glass of cum`)
+    : window.alert(`Congrats, you made nothing!`);
 }, 75);
