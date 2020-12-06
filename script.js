5bbc9e023e04887e6ea2512a0f69ffb523189148
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-05 22:02:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,34 +23,6 @@ setTimeout(function () {
   } else {
     window.alert(`Congrats, you made nothing!`);
   }
-
-  const ui = {
-  confirm: async (message) => createConfirm(message)
-}
-
-const createConfirm = (message) => {
-  return new Promise((complete, failed)=>{
-    $('#confirmMessage').text(message)
-
-    $('#confirmYes').off('click');
-    $('#confirmNo').off('click');
-    
-    $('#confirmYes').on('click', ()=> { $('.confirm').hide(); complete(true); });
-    $('#confirmNo').on('click', ()=> { $('.confirm').hide(); complete(false); });
-    
-    $('.confirm').show();
-  });
-}
-                     
-const saveForm = async () => {
-  const confirm = await ui.confirm('Are you sure you want to do this?');
-  
-  if(confirm){
-    alert('yes clicked');
-  } else{
-    alert('no clicked');
-  }
-}
  
 
 }, 75);
