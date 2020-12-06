cd3bd736de551a4cc9f20371f0129f796360440a
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-05 20:55:28 -0500

    Updating script.js
    bryh

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let apples = prompt(`Enter number of apples`)
+let apples = prompt(`Enter number of apples`);
 let oranges = prompt(`Enter amount of oranges`);
 let cum = prompt(`Enter amount of cum in liters`);
 
@@ -16,6 +16,6 @@ if (apples > oranges && oranges == 0 && cum == 0) {
   window.alert(`You made orange-cum juice! You will get pregante!`);
 } else if (apples == 0 && oranges == 0 && cum > 0{
 window.alert(`You made unpasteurized milk`);
-else {
+}else {
   window.alert(`Congrats, you made nothing!`);
 }
