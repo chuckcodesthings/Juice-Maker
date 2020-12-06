064e625c3021182d6343ed62be60085707095c0a
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-05 20:10:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let oranges = prompt(`Enter amount of oranges`);
 
 if (apples > oranges && oranges == 0) {
   window.alert(`You made apple juice!`);
-} else if (apples > 0 && oranges >= 0) {
+} else if (apples > 0 && oranges == 0) {
   window.alert(`You made apple-orange juice!`);
-} else if (apples == 0 && oranges >= 0) {
+} else if (apples == 0 && oranges > 0) {
   window.alert(`You made orange juice!`);
 } else {
   window.alert(`Congrats, you made nothing!`);
