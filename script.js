49db8f9a7e4d5ffc9635a8bc5e70e60572c1b0d2
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-05 21:26:42 -0500

    Finished v1
    kek

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-setTimeout(function(){
-   
-let apples = prompt(`Enter number of apples`);
-let oranges = prompt(`Enter amount of oranges`);
-let cum = prompt(`Enter amount of cum in liters`);
- 
-}, 500);
+'use strict'
+setTimeout(function () {
 
-if (apples > oranges && oranges == 0 && cum == 0) {
-  window.alert(`You made apple juice!`);
-} else if (apples > 0 && oranges >= 0 && cum == 0) {
-  window.alert(`You made apple-orange juice!`);
-} else if (apples == 0 && oranges > 0 && cum == 0) {
-  window.alert(`You made orange juice!`);}
-  else if (apples > oranges && oranges == 0 && cum > 0) {
-  window.alert(`You made apple juice, but it strangely tastes like the milk your uncle gives you when you visit him!`);
-} else if (apples > 0 && oranges >= 0 && cum > 0) {
-  window.alert(`You made apple-orange juice, it has a noticeable cum aftertaste!`);
-} else if (apples == 0 && oranges > 0 && cum > 0) {
-  window.alert(`You made orange-cum juice! You will get pregante!`);
-} else if (apples == 0 && oranges == 0 && cum > 0{
-window.alert(`You made unpasteurized milk`);
-}else {
-  window.alert(`Congrats, you made nothing!`);
-}
+  let apples = prompt(`Enter number of apples`);
+  let oranges = prompt(`Enter amount of oranges`);
+  let cum = prompt(`Enter amount of cum in liters`);
+
+
+  if (apples > oranges && oranges == 0 && cum == 0) {
+    window.alert(`You made apple juice!`);
+  } else if (apples > 0 && oranges >= 0 && cum == 0) {
+    window.alert(`You made apple-orange juice!`);
+  } else if (apples == 0 && oranges > 0 && cum == 0) {
+    window.alert(`You made orange juice!`);
+  } else if (apples > oranges && oranges == 0 && cum > 0) {
+    window.alert(`You made apple juice, but it strangely tastes like the milk your uncle gives you when you visit him!`);
+  } else if (apples > 0 && oranges > 0 && cum > 0) {
+    window.alert(`You made apple-orange juice, but it has a noticeable cum aftertaste!`);
+  } else if (apples == 0 && oranges > 0 && cum > 0) {
+    window.alert(`You mixed semen with orange juice, go to hell!`);
+  } else if (apples == 0 && oranges == 0 && cum > 0) {
+    window.alert(`You made a delicious glass of cum`);
+  } else {
+    window.alert(`Congrats, you made nothing!`);
+  }
+
+}, 75);
+
+
