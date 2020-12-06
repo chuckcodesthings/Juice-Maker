398999788a5be5838c163fb0edfe678092261a14
Charles Rivard <61912888+chuckcodesthings@users.noreply.github.com>
2020-12-05 20:41:36 -0500

    BABABOOEY
    HEHEH

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let apples = prompt(`Enter number of apples`)
+let oranges = prompt(`Enter amount of oranges`);
+
+if (apples > oranges && oranges == 0) {
+  window.alert(`You made apple juice!`);
+} else if (apples > 0 && oranges >= 0) {
+  window.alert(`You made apple-orange juice!`);
+} else if (apples == 0 && oranges > 0) {
+  window.alert(`You made orange juice!`);
+} else {
+  window.alert(`Congrats, you made nothing!`);
+}
